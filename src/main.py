3f0a7454e498cf56c9089738b5f975f8455b3311
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-10-13 17:26:37 +0200

    remove invalid fallback

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 from typing import Any
 
 import pandas as pd
+import sqlalchemy
 from pandas import DataFrame
 from dune_client.client import DuneClient
 from dune_client.query import QueryBase
@@ -10,8 +11,7 @@ from dotenv import load_dotenv
 
 from src.types import DUNE_TO_PG
 
-load_dotenv()
-DUNE_API_KEY = os.environ.get("DUNE_API_KEY", "INVALID_DUNE_API_KEY")
+DUNE_API_KEY = os.environ.get("DUNE_API_KEY")
 DB_URL = os.environ.get("DB_URL")
 
 # TODO(bh2smith): parse config file for most of the following stuff
@@ -31,12 +31,8 @@ def reformat_varbinary_columns(
     return df
 
 
-def fetch_dune_data(query_id: int) -> tuple[DataFrame, DataTypes]:
-    result = (
-        DuneClient(DUNE_API_KEY, performance=QUERY_ENGINE)
-        .run_query(query=QueryBase(query_id), ping_frequency=POLL_FREQUENCY)
-        .result
-    )
+def fetch_dune_data(dune: DuneClient, query: QueryBase) -> tuple[DataFrame, DataTypes]:
+    result = dune.run_query(query, ping_frequency=POLL_FREQUENCY).result
     if result is None:
         raise ValueError("Query execution failed!")
 
@@ -52,19 +48,27 @@ def fetch_dune_data(query_id: int) -> tuple[DataFrame, DataTypes]:
     return df, dtypes
 
 
-def save_to_postgres(df: DataFrame, dtypes: DataTypes) -> None:
-    db_connection = DB_URL
-    engine = create_engine(db_connection)
+def save_to_postgres(
+    engine: sqlalchemy.engine.Engine, df: DataFrame, dtypes: DataTypes
+) -> None:
     df.to_sql(TABLE_NAME, engine, if_exists="replace", index=False, dtype=dtypes)
     print("Data saved to PostgreSQL successfully!")
 
 
 def main() -> None:
+    load_dotenv()
+    if DUNE_API_KEY is None:
+        raise EnvironmentError("DUNE_API_KEY environment variable must be set!")
     if QUERY_ID is None:
         raise EnvironmentError("QUERY_ID must be set")
-    df, types = fetch_dune_data(int(QUERY_ID))
+
+    df, types = fetch_dune_data(
+        dune=DuneClient(DUNE_API_KEY, performance=QUERY_ENGINE),
+        query=QueryBase(int(QUERY_ID)),
+    )
     if df is not None:
-        save_to_postgres(df, types)
+        engine = create_engine(DB_URL)
+        save_to_postgres(engine, df, types)
 
 
 if __name__ == "__main__":
