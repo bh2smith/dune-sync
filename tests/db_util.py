3ecb06766aab541adfffbef9b57f8d9f4c12e5e5
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-10-26 17:58:34 +0200

    delete dead code

--- tests/db_util.py
+++ tests/db_util.py
@@ -10,16 +10,9 @@ def query_pg(engine: sqlalchemy.engine.Engine, query_str: str) -> list[dict[str,
 
     return [
         {
-            # key: (bytes(value) if isinstance(value, memoryview) else value)
+            # Convert memoryview to hex strings: could also use bytes(value)
             key: (f"0x{value.hex()}" if isinstance(value, memoryview) else value)
             for key, value in zip(result.keys(), row)
         }
         for row in rows
     ]
-
-
-# def query_pg(engine: sqlalchemy.engine.Engine, query_str: str) -> list[dict[str, Any]]:
-#     with engine.connect() as connection:
-#         result = connection.execute(text(query_str))
-#         rows = result.fetchall()
-#     return [dict(zip(result.keys(), row)) for row in rows]
