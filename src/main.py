9b5bbe49edfc8ba0cc69751119f7d73b6dbd6c49
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-11-02 22:46:32 +0100

    Basic Configuration for LocalToDune Jobs

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from src.config import Env, RuntimeConfig
 from src.dune_to_local.postgres import dune_to_postgres
+from src.local_to_dune.postgres import postgres_to_dune
 
 
 def main() -> None:
     env = Env.load()
     config = RuntimeConfig.load_from_toml("config.toml")
     # TODO: Async job execution https://github.com/bh2smith/dune-sync/issues/20
-    for job in config.jobs:
-        dune_to_postgres(env, job)
+    for d2l_job in config.dune_to_local_jobs:
+        dune_to_postgres(env, d2l_job)
+    for l2d_job in config.local_to_dune_jobs:
+        postgres_to_dune(env, l2d_job)
 
 
 if __name__ == "__main__":
