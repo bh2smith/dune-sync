82ed52aafc571fdc3a931b1bb60f0addef8964cf
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-11-04 23:27:20 +0100

    Minor Cleanup Naming & Imports (#42)
    1. Many of the declared types are only used in one place (removed).
    2. Delete sync type, putting the one entry into `interfaces.py`.
    3. Rename `BaseJob` as `Job` because there is only one Job.

--- src/sync_types.py
+++
@@ -1,8 +0,0 @@
-from typing import Any, Literal
-
-from pandas import DataFrame
-
-TypedDataFrame = tuple[DataFrame, dict[str, Any]]
-
-TableExistsPolicy = Literal["append", "replace"]
-DuneQueryEngine = Literal["medium", "large"]
