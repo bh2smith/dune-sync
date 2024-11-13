446e01a7495f8a09fd84789ccc5369aeb57eae8b
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-11-13 08:12:35 +0100

    Treat Unknown Types as JSONB (#64)

--- src/sources/dune.py
+++ src/sources/dune.py
@@ -3,6 +3,7 @@ Source logic for Dune Analytics.
 """
 
 from abc import ABC
+import json
 from typing import Type, Any, Literal
 
 import pandas as pd
@@ -10,10 +11,11 @@ from dune_client.client import DuneClient
 from dune_client.models import ExecutionResult
 from dune_client.query import QueryBase
 from pandas import DataFrame
-from sqlalchemy import BIGINT, BOOLEAN, VARCHAR, DATE, TIMESTAMP
-from sqlalchemy.dialects.postgresql import BYTEA, DOUBLE_PRECISION, INTEGER
+from sqlalchemy import BIGINT, BOOLEAN, VARCHAR, DATE, TIMESTAMP, NUMERIC
+from sqlalchemy.dialects.postgresql import BYTEA, DOUBLE_PRECISION, INTEGER, JSONB
 
 from src.interfaces import Source, TypedDataFrame
+from src.logger import log
 
 DUNE_TO_PG: dict[str, Type[Any]] = {
     "bigint": BIGINT,
@@ -24,6 +26,7 @@ DUNE_TO_PG: dict[str, Type[Any]] = {
     "varchar": VARCHAR,
     "double": DOUBLE_PRECISION,
     "timestamp with time zone": TIMESTAMP,
+    "uint256": NUMERIC,
     # TODO: parse these innards more dynamically.
     # "decimal(38, 0)": NUMERIC(38, 0),
     # "array(varbinary)": ARRAY(BYTEA),
@@ -54,6 +57,12 @@ def _reformat_varbinary_columns(
     return df
 
 
+def _reformat_unknown_columns(df: DataFrame, unknown_columns: list[str]) -> DataFrame:
+    for col in unknown_columns:
+        df[col] = df[col].apply(json.dumps)
+    return df
+
+
 def dune_result_to_df(result: ExecutionResult) -> TypedDataFrame:
     """
     Converts a Dune query result into a DataFrame with PostgreSQL-compatible data types.
@@ -73,15 +82,20 @@ def dune_result_to_df(result: ExecutionResult) -> TypedDataFrame:
         mapping column names to PostgreSQL-compatible data types.
     """
     metadata = result.metadata
-    dtypes, varbinary_columns = {}, []
+    dtypes, varbinary_columns, unknown_columns = {}, [], []
     for name, d_type in zip(metadata.column_names, metadata.column_types):
-        dtypes[name] = DUNE_TO_PG[d_type]
+        dtypes[name] = DUNE_TO_PG.get(d_type)
+        if dtypes[name] is None:
+            log.warning("Unknown column: %s - treating as JSONB", d_type)
+            unknown_columns.append(name)
+            dtypes[name] = JSONB
         if d_type == "varbinary":
             varbinary_columns.append(name)
 
     df = pd.DataFrame(result.rows)
     # escape bytes
     df = _reformat_varbinary_columns(df, varbinary_columns)
+    df = _reformat_unknown_columns(df, unknown_columns)
     return df, dtypes
 
 
