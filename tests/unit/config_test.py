c0c0084c47e72201bc2f8109daed4ded4a89bab6
mooster531 <15d1dc293d2e@proton.me>
2024-11-27 19:06:29 +0100

    Allow for env-var substitution inside query_string and table_name (#98)
    Closes #94

--- tests/unit/config_test.py
+++ tests/unit/config_test.py
@@ -5,7 +5,8 @@ from unittest.mock import patch
 
 from dune_client.types import QueryParameter
 
-from src.config import Env, RuntimeConfig, parse_query_parameters
+from src.config import Env, RuntimeConfig
+from src.sources.dune import parse_query_parameters
 from tests import config_root
 
 
@@ -47,6 +48,11 @@ class TestRuntimeConfig(unittest.TestCase):
             {
                 "DUNE_API_KEY": "test_key",
                 "DB_URL": "postgresql://postgres:postgres@localhost:5432/postgres",
+                "Query_ID": "123321",
+                "POLL_FREQUENCY_DUNE_PG": "192",
+                "BLOCKCHAIN_NAME": "moosis",
+                "WHAT_IF_EXISTS": "replace",
+                "table_name": "my_pg_table",
             },
             clear=True,
         )
@@ -58,32 +64,54 @@ class TestRuntimeConfig(unittest.TestCase):
 
     def test_load_basic_conf(self):
         config_file = config_root / "basic.yaml"
-        conf = RuntimeConfig.load_from_yaml(config_file.absolute())
+        conf = RuntimeConfig.load(config_file.absolute())
         self.assertEqual(2, len(conf.jobs))
-        # TODO: come up with more explicit assertions.
+        dune_to_pg_job = conf.jobs[0]
+        pg_to_dune_job = conf.jobs[1]
+        self.assertEqual("test_key", dune_to_pg_job.source.client.token)
+        self.assertEqual(
+            "postgresql://postgres:***@localhost:5432/postgres",
+            str(pg_to_dune_job.source.engine.url),
+        )
+
+    def test_load_templated_conf(self):
+        config_file = config_root / "basic_with_env_placeholders.yaml"
+        conf = RuntimeConfig.load(config_file.absolute())
+        self.assertEqual(2, len(conf.jobs))
+        dune_to_pg_job = conf.jobs[0]
+        pg_to_dune_job = conf.jobs[1]
+        self.assertEqual(int("123321"), dune_to_pg_job.source.query.query_id)
+        self.assertEqual(int("192"), dune_to_pg_job.source.poll_frequency)
+        self.assertEqual("moosis", dune_to_pg_job.source.query.params[0].value)
+        self.assertEqual("replace", dune_to_pg_job.destination.if_exists)
+        self.assertEqual("my_pg_table", pg_to_dune_job.destination.table_name)
+
+        config_file = config_root / "basic_with_env_missing_vars.yaml"
+        with self.assertRaises(KeyError):
+            RuntimeConfig.load(config_file.absolute())
 
     def test_load_unsupported_conf(self):
         with self.assertRaises(ValueError) as context:
-            RuntimeConfig.load_from_yaml(config_root / "unsupported_source.yaml")
+            RuntimeConfig.load(config_root / "unsupported_source.yaml")
         self.assertIn("Unsupported source_db type", str(context.exception))
 
         with self.assertRaises(ValueError) as context:
-            RuntimeConfig.load_from_yaml(config_root / "unsupported_dest.yaml")
+            RuntimeConfig.load(config_root / "unsupported_dest.yaml")
         self.assertIn("Unsupported destination_db type", str(context.exception))
 
     def test_load_buggy_conf(self):
         with self.assertRaises(KeyError) as context:
-            RuntimeConfig.load_from_yaml(config_root / "buggy.yaml")
+            RuntimeConfig.load(config_root / "buggy.yaml")
         self.assertIn("'table_name'", str(context.exception))
 
         with self.assertRaises(SystemExit):
-            RuntimeConfig.load_from_yaml(config_root / "unknown_src.yaml")
+            RuntimeConfig.load(config_root / "unknown_src.yaml")
 
         with self.assertRaises(SystemExit):
-            RuntimeConfig.load_from_yaml(config_root / "unknown_dest.yaml")
+            RuntimeConfig.load(config_root / "unknown_dest.yaml")
 
         with self.assertRaises(SystemExit):
-            RuntimeConfig.load_from_yaml(config_root / "no_data_sources.yaml")
+            RuntimeConfig.load(config_root / "no_data_sources.yaml")
 
 
 class TestParseQueryParameters(unittest.TestCase):
